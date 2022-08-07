2ff235c1235af3c855650075aee9eb3000f8c1a6
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-07 02:52:17 -0400

    javaScript 7

--- app.js
+++ app.js
@@ -30,8 +30,12 @@ form.addEventListener('submit', function (e) {
   //-infinito,-1
   //9, infinito+
   if(isNaN(value) || value < 0 || value > 9) {
-    result.innerHTML = `<p class="result">${text[random]}</p>`
+    result.innerHTML = `<p class="result">${text[random]}</p>`;
+  } else {
+    let tempText = text.slice(0, value);
+    tempText = tempText.map(function(item) {
+      return `<p class="result">${item}</p>`
+    }).join("");
+    result.innerHTML = tempText;
   }
-
-
 });
